e03d066a4f3e2825c455af6b366cb79080a5a049
Greenbeard Geordi <ggeordi@oa-n2.21-school.ru>
2019-09-08 17:27:10 +0300

    Update

--- ex27/srcs/main.c
+++ ex27/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ggeordi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/06 18:54:31 by ggeordi           #+#    #+#             */
-/*   Updated: 2019/09/06 19:10:37 by ggeordi          ###   ########.fr       */
+/*   Updated: 2019/09/08 17:26:56 by ggeordi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 int		main(int argc, char **argv)
 {
 	if (argc == 1)
-		write(2, "File name missing.\n", 18);
+		write(2, "File name missing.\n", 19);
 	else if (argc > 2)
-		write(2, "Too many arguments.\n", 19);
+		write(2, "Too many arguments.\n", 20);
 	else
 		ft_display_file(argv[1]);
 	return (1);
